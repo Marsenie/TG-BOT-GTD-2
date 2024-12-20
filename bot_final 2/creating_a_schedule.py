01592eaacbb6b8a6d76f5ee15123f035f1c92896
abookvv <97209148+abookvv@users.noreply.github.com>
2024-12-20 22:50:04 +0500

    Update creating_a_schedule.py

--- bot_final 2/creating_a_schedule.py
+++ bot_final 2/creating_a_schedule.py
@@ -1,3 +1,18 @@
-class tabl():
+import numpy as np
+from matplotlib import pyplot as plt
+
+class Graph:
   def __init__(self, task_week_last, task_week_past):
-    pass
+    self.task_week_past = task_week_past
+    self.task_week_last = task_week_last
+    create_graph()
+
+  def create_graph():
+    plt.figure(figsize=(10, 4))
+    x = ['last week', 'this week']
+    y = [self.task_week_last, self.task_week_past]
+    plt.scatter(x, y)
+    plt.title('График выполненных задач за прощедшие две недели')
+    plt.grid(True)
+    plt.show()
+    plt.savefig('graphic.png', bbox_inches='tight')
