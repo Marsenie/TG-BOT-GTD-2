c35bee74f4080842801a0d9f5899c2f1d6723f03
abookvv <97209148+abookvv@users.noreply.github.com>
2024-12-20 23:25:44 +0500

    Update creating_a_schedule.py

--- bot_final 2/creating_a_schedule.py
+++ bot_final 2/creating_a_schedule.py
@@ -1,20 +1,45 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-#этот класс создает график показывающий количество выполненных задач за прощедщие две недели, чтобы сравнить продуктивность
-class Graph:
-  def __init__(self, task_week_last, task_week_past):
-    self.task_week_past = task_week_past
-    self.task_week_last = task_week_last
-    create_graph()
+#тут мы создаем свой декоратор для валидациизначений о задачах, выполненных за посление две недели, для создания графика
+class BaseCustomException(Exception):
+    """Базовое пользовательское исключение."""
+    pass
 
-  #создаем график и сохраняем его картинкой png
-  def create_graph():
-    plt.figure(figsize=(10, 4))
-    x = ['last week', 'this week']
-    y = [self.task_week_last, self.task_week_past]
-    plt.scatter(x, y)
-    plt.title('График выполненных задач за прощедшие две недели')
-    plt.grid(True)
-    plt.show()
-    plt.savefig('graphic.png', bbox_inches='tight')
+class InvalidValueError(BaseCustomException):
+    """Исключение для случаев некорректного значения."""
+    def __init__(self, value, message="Некорректное значение"):
+        self.value = value
+        self.message = message
+        super().__init__(f"{message}: {value}")
+
+
+def validate_value(condition, error_message="Некорректное значение"):
+    """Декоратор для проверки значения аргументов."""
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for arg in args[1:]:
+                if not condition(arg):
+                    raise InvalidValueError(arg, error_message)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+  
+#тут мы создаем график, на котором показанно сколько задач пользователь сделал за последние две недели
+class Graph:
+    def __init__(self, task_week_last, task_week_past):
+        self.task_week_last = task_week_last
+        self.task_week_past = task_week_past
+        self.create_graph()
+
+    @validate_value(lambda x: isinstance(x, int), "Аргумент должен быть целым числом")
+    def create_graph(self):
+        plt.figure(figsize=(8, 4))
+        x = ['Last Week', 'This Week']
+        y = [self.task_week_last, self.task_week_past]
+        plt.bar(x, y)
+        plt.title('График выполненных задач за последние две недели')
+        plt.savefig('graphic.png', bbox_inches='tight') 
+        plt.show()
+
+
