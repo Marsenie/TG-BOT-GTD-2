ce07e4f00d95487dd2f5dd31483b253aa8b01631
Marsenie <ars.rys.ry@gmail.com>
2024-12-30 20:22:35 +0300

    Update exceptions.py

--- bot_final 2/exceptions.py
+++ bot_final 2/exceptions.py
@@ -1,3 +1,27 @@
-class TgExceptions(Exception):
-    def __init__(self, text):
-        self.txt = text
+"""самописные ошибки"""
+class BaseCustomException(Exception):
+    """Базовое пользовательское исключение."""
+    pass
+
+
+class InvalidValueError(Exception):
+    """Исключение для случаев некорректного значения."""
+    def __init__(self, value, message="Некорректное значение"):
+        self.value = value
+        self.message = message
+        super().__init__(f"{message}: {value}")
+
+
+class CreateFileErr(Exception):
+    def __init__(self, mgs, info):
+        """Исклечение при ошибке создания файла"""
+        super().__init__(mgs)
+        self.info = info
+
+
+class ReadErr(Exception):
+    def __init__(self, mgs, info):
+        """Исклечение при ошибке чтения файла"""
+        super().__init__(mgs)
+        self.info = info
+
